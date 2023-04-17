21fce693261e99e400c3d1db60982d6533d8752d
Not Sam <samuelbunker@live.com>
2023-04-17 11:31:38 -0400

    History Tour Website Push
    Pushing the history tour of the website

---
+++ data.js
@@ -0,0 +1,461 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-corner-of-high-and-second-streets",
+      "name": "Corner of High and Second Streets",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.0022765164156428597,
+        "pitch": -0.2717861058881006,
+        "fov": 1.2344983363832869
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6743833587359696,
+          "pitch": 0.061227134742605216,
+          "rotation": 0.7853981633974483,
+          "target": "1-the-williamsburg-hotel"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.0022765164156428597,
+          "pitch": -0.2717861058881006,
+          "title": "Dean Theater",
+          "text": "<div>&nbsp;Towards the middle of W Second Street, you will see a building with a half circle stained glass window. Built in 1915, this was the location of the Dean Theater. The window is original, and there used to be a ticket booth in front of the building. Silent movies were the first to be shown there using a player piano for background music. A fire in 1936 destroyed much of the inside of the building but repairs were made and the theater continued its operation. With the coming of television, it closed permanently in the 1950s.</div><a href='https://goo.gl/maps/L96ADpMrdQuYuZMS7' target='_blank'><img src='./img/maps.png' alt='Google Maps Image' style='position:relative;left:50%;transform:translateX(-50%);margin-top:10px;height:100px;width:100px;'></a>"
+        }
+      ]
+    },
+    {
+      "id": "1-the-williamsburg-hotel",
+      "name": "The Williamsburg Hotel",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.23448119081140817,
+        "pitch": -0.20337838523662555,
+        "fov": 1.2344983363832869
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0385900515122142,
+          "pitch": 0.057915231584329874,
+          "rotation": 12.566370614359176,
+          "target": "2-corner-of-high-street-and-sage-hill-drive"
+        },
+        {
+          "yaw": -2.483867160569467,
+          "pitch": 0.14636576811110658,
+          "rotation": 0,
+          "target": "0-corner-of-high-and-second-streets"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.4615069491705821,
+          "pitch": -0.04892288103407694,
+          "title": "The Williamsburg Hotel",
+          "text": "<div>Located on High Street is a large yellow brick building known as the Williamsburg Hotel. It was built by Charles Schwab as a place for investors to stay when they came to town. He was born in on the outskirts of Williamsburg and made his fortune in the steel industry. Because of his love for this town, he persuaded individuals to invest a total of $1,000,000 towards building a paper mill for Williamsburg once the iron ore industry ended.&nbsp;</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-corner-of-high-street-and-sage-hill-drive",
+      "name": "Corner of High Street and Sage Hill Drive",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.4630257341394568,
+        "pitch": -0.06691816938951156,
+        "fov": 1.2344983363832869
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.058785522064139,
+          "pitch": -0.061133001277646315,
+          "rotation": 0,
+          "target": "3-corner-of-plum-street-and-sage-hill-drive"
+        },
+        {
+          "yaw": -2.2156084608977906,
+          "pitch": 0.03660535409721177,
+          "rotation": 0.7853981633974483,
+          "target": "1-the-williamsburg-hotel"
+        },
+        {
+          "yaw": -0.49032489744000607,
+          "pitch": 0.055251202571639624,
+          "rotation": 0.7853981633974483,
+          "target": "4-presbyterian-cemetery---408-liberty-street"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.04290878109941332,
+          "pitch": 0.030298373266523626,
+          "title": "The Big Spring",
+          "text": "The Big Spring which is what drew Jacob Ake to this area in 1790 from Maryland."
+        },
+        {
+          "yaw": 0.7679588949413869,
+          "pitch": 0.12892966020439545,
+          "title": "Image of The Big Spring",
+          "text": "<img src='./img/TheBigSpring.jpg' alt='Beautiful picture of the big springs close up.' style='width:100%;height:180px;'>"
+        },
+        {
+          "yaw": 2.5529890238131108,
+          "pitch": -0.08220583677000803,
+          "title": "Ironmaster's House",
+          "text": "This large white house across the street was the home of iron master John Neff during the time when the area around Williamsburg was popular for its many iron furnaces. The iron fencing surrounding this home is original, and was locally made in the foundry"
+        },
+        {
+          "yaw": -1.9376919630698026,
+          "pitch": 0.1057024907370483,
+          "title": "Former Schoolhouse",
+          "text": "<div>High Street was home to a school building, which was destroyed by fire in 1940.</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-corner-of-plum-street-and-sage-hill-drive",
+      "name": "Corner of Plum Street and Sage Hill Drive",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.155835621567176,
+        "pitch": 0.022241034732356724,
+        "fov": 1.2344983363832869
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.291406261419489,
+          "pitch": 0.09067558547186039,
+          "rotation": 0,
+          "target": "2-corner-of-high-street-and-sage-hill-drive"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6356516152973297,
+          "pitch": 0.042173056955018495,
+          "title": "Corner of Plum Street and Sage Hill Drive",
+          "text": "Here is the cemetery where Jacob Ake, the founder of Aketown and his wife are buried. Aketown was renamed Williamsburg in honor of his son, William."
+        },
+        {
+          "yaw": -3.000324770983749,
+          "pitch": -0.07806378793860524,
+          "title": "Williamsburg School District",
+          "text": "<div>The present day high school and elementary school are visible from this spot. Williamsburg High School is the oldest in Blair County. Jacob Ake was a firm believer that all children should have an education. He paid the teachers and bought supplies from his own pocket.&nbsp;</div>"
+        },
+        {
+          "yaw": -0.5733918931153035,
+          "pitch": 0.007680700027421494,
+          "title": "Former Schoolhouse",
+          "text": "This brick house was one of the first schools in Williamsburg for students beyond 6th grade."
+        }
+      ]
+    },
+    {
+      "id": "4-presbyterian-cemetery---408-liberty-street",
+      "name": "Presbyterian Cemetery - 408 Liberty Street",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.3278616537326187,
+        "pitch": 0.004561534182586868,
+        "fov": 1.2344983363832869
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.687658587956525,
+          "pitch": 0.17801908764260155,
+          "rotation": 0,
+          "target": "2-corner-of-high-street-and-sage-hill-drive"
+        },
+        {
+          "yaw": -1.442520875847059,
+          "pitch": 0.09214825269662619,
+          "rotation": 0.7853981633974483,
+          "target": "5-corner-of-liberty-and-east-third-streets"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6341860839486912,
+          "pitch": -0.0028309037915459356,
+          "title": "Wilmer Stultz",
+          "text": "<div>Located towards the top of Williamsburg’s Presbyterian Cemetery, underneath the group of large trees you will find several stones with the name “Stultz”. Among these lies the final resting place of Williamsburg native Mr. Wilmer Stultz. His stone details information about his flight across the Atlantic Ocean carrying a very special passenger: Amelia Earhart. Along with his friend Lou Gordon, the three flew in June 1928, and made Earhart the first woman to fly across the Atlantic nonstop</div>"
+        },
+        {
+          "yaw": 0.990808407693109,
+          "pitch": 0.20919388839120145,
+          "title": "Wilmer Stultz Tombstone Head",
+          "text": "<img src='./img/WilmerStultz.jpg' alt='1900 to 1929. In Memory of Wilmer L. Stultz. Major, National Guard Air Corps, Captain, U.S. Army Air Corps Reserve, 634,Aero Squandron 1917-1919, U. S. Naval Air Serice 1919 - 1922, Pilot 'FriendShip' Trans-Atlantic Flight Trepassey, N. F. To Burry Port, Wales, June 17th & 18th, 1928.' style='width:100%;height:auto;'>"
+        }
+      ]
+    },
+    {
+      "id": "5-corner-of-liberty-and-east-third-streets",
+      "name": "Corner of Liberty and East Third Streets",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.2560705769841203,
+        "pitch": 0.01858503636140263,
+        "fov": 1.2344983363832869
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.072019813703717,
+          "pitch": 0.019486591496040262,
+          "rotation": 7.0685834705770345,
+          "target": "4-presbyterian-cemetery---408-liberty-street"
+        },
+        {
+          "yaw": 2.9568834434671043,
+          "pitch": 0.11267877562233863,
+          "rotation": 0,
+          "target": "6-corner-of-east-second-and-spring-streets-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.00526577652939153,
+          "pitch": 0.11119498971036279,
+          "title": "Schwabtown",
+          "text": "<div>If you look at the houses in this part of town, you will notice the outside appearances of most are very similar. This area used to house the Ake farm, which was purchased by Charles Schwab shortly after construction began on the paper mill. “Schwabtown” as it was known, was built because Schwab knew that families coming to work at the mill would need housing. About 50 houses were built for people to buy or rent.&nbsp;</div>"
+        },
+        {
+          "yaw": -1.3046764826774204,
+          "pitch": 0.004620291544519262,
+          "title": "Former Stable",
+          "text": "This apartment building was formerly the stable for horses and mules during the canal era.&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "6-corner-of-east-second-and-spring-streets-",
+      "name": "Corner of East Second and Spring Streets ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.8011378099888695,
+        "pitch": -0.026860909853105852,
+        "fov": 1.2344983363832869
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.733500440517746,
+          "pitch": 0.06886986452228605,
+          "rotation": 7.0685834705770345,
+          "target": "6-corner-of-east-second-and-spring-streets-"
+        },
+        {
+          "yaw": -1.314388358577709,
+          "pitch": -0.011644991154922124,
+          "rotation": 7.0685834705770345,
+          "target": "7-corner-of-front-and-high-streets"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.0548761819260744,
+          "pitch": -0.07494663303362081,
+          "title": "Williamsburg Borough Office",
+          "text": "<div>What is now home to the fire company and Williamsburg Borough Office was once the site of a grist mill. It had two water wheels that were powered by the Big Spring, which produced 6 million gallons of water each day, as it continued on its path to the river. The water wheels operated the large grist stones inside the mill that crushed various grains into flour.</div>"
+        },
+        {
+          "yaw": 0.017396292283308412,
+          "pitch": -0.022362300668774537,
+          "title": "Williamsburg Foundry",
+          "text": "This building has been home to several different businesses over the years. During the height of the iron industry, it housed a foundry making various products from iron. It was home to hoop factory where the metal hoops for wooden barrels were made. It was at one time a broom factory. For over 100 years, it served as a hardware store for the people of Williamsburg. It is now an apartment."
+        }
+      ]
+    },
+    {
+      "id": "7-corner-of-front-and-high-streets",
+      "name": "Corner of Front and High Streets",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.4044148143408446,
+        "pitch": -0.02317453366219091,
+        "fov": 1.2344983363832869
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4278550253819553,
+          "pitch": 0.056592232342781656,
+          "rotation": 12.566370614359176,
+          "target": "6-corner-of-east-second-and-spring-streets-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.23607666741072109,
+          "pitch": 0.044550183902945406,
+          "title": "The Lower Trail",
+          "text": "<div>The trail that people use was actually the path the mules walked on while pulling canal boats on the Frankstown Branch of the Juniata River during PA’s Canal Era. The canal boats passed through Williamsburg from Eastern PA on their way to Hollidaysburg, and to the Allegheny Portage Passage where they would cross the mountains and continue down to Johnstown. When the canal system was abandoned, the mule path served as the base for the tracks of the Pennsylvania RR.&nbsp;</div>"
+        },
+        {
+          "yaw": 0.6192644192496477,
+          "pitch": -0.0327453139624172,
+          "title": "Former Railroad Station",
+          "text": "The space that Martin’s currently occupies was once the home of a railroad station.&nbsp;"
+        }
+      ]
+    }
+  ],
+  "name": "Williamsburg History Tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
